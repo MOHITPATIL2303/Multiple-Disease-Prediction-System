cd08820f12add41329c6fa68ccc0336d0416798a
MOHITPATIL2303 <mohitpatil2332005@gmail.com>
2025-11-16 13:59:05 +0530

    Multiple Disease Prediction System.py

--- Multiple Disease Prediction System.py
+++ Multiple Disease Prediction System.py
@@ -14,11 +14,11 @@ working_dir = os.path.dirname(os.path.abspath(__file__))
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('C:/Users/Mohit\OneDrive\Desktop\Multiple Disease Prediction System using Machine Learning/saved_models/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('C:/Users/Mohit\OneDrive\Desktop\Multiple Disease Prediction System using Machine Learning/saved_models/heart_disease_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
 
-parkinsons_model = pickle.load(open('C:/Users/Mohit\OneDrive\Desktop\Multiple Disease Prediction System using Machine Learning/saved_models/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
 # sidebar for navigation
 with st.sidebar:
@@ -246,5 +246,6 @@ if selected == "Parkinsons Prediction":
             parkinsons_diagnosis = "The person has Parkinson's disease"
         else:
             parkinsons_diagnosis = "The person does not have Parkinson's disease"
+
 
     st.success(parkinsons_diagnosis)
